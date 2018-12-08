09289e9e2f0b0a551606ee8afb7bedcb52785528
gdm <gdm.mbp@gdmMBP.local>
2018-12-08 14:20:42 -0500

    Add Firebase login auth

--- Breakpoint/Controller/AuthVC.swift
+++ Breakpoint/Controller/AuthVC.swift
@@ -17,14 +17,15 @@ class AuthVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func signInWithEmailBtnPressed(_ sender: Any) {
+        let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginVC")
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func facebookSignInBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func googleSignInBtnPressed(_ sender: Any) {
+    }
 }
