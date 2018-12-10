dfa249a000de864bcf1aa110478b11b493230f3b
gdm <gdm.mbp@gdmMBP.local>
2018-12-09 19:47:07 -0500

    Add CreatePostVC and post send to Firebase functionality.

---
+++ Breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,54 @@
+//
+//  CreatePostVC.swift
+//  Breakpoint
+//
+//  Created by gdm on 12/9/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+        
+        
+    }
+    
+
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete:  { (isComplete) in
+                if isComplete {
+                self.sendButton.isEnabled = true
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                self.sendButton.isEnabled = true
+                print("There was an error")
+            }
+        })
+        }
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
