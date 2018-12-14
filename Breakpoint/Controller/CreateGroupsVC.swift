d619078eb0f651effa8e2906c1fdc9541514f0d1
gdm <gdm.mbp@gdmMBP.local>
2018-12-13 19:54:13 -0500

    Add user search for add groups

--- Breakpoint/Controller/CreateGroupsVC.swift
+++ Breakpoint/Controller/CreateGroupsVC.swift
@@ -17,17 +17,79 @@ class CreateGroupsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var doneButton: UIButton!
     
+    var emailArray = [String]()
+    var chosenUserArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+       emailSearchTextField.delegate = self
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        doneButton.isHidden = true
+    }
+    
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextField.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     @IBAction func doneButtonPressed(_ sender: Any) {
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
+    
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? UserCell else { return }
+        if !chosenUserArray.contains(cell.emailLabel.text!) {
+            chosenUserArray.append(cell.emailLabel.text!)
+            groupMemberLbl.text = chosenUserArray.joined(separator: ", ")
+            doneButton.isHidden = false
+        } else {
+            chosenUserArray = chosenUserArray.filter({ $0 != cell.emailLabel.text! })
+            if chosenUserArray.count >= 1 {
+                groupMemberLbl.text = chosenUserArray.joined(separator: ", ")
+            } else {
+                groupMemberLbl.text = "add people to your group"
+                doneButton.isHidden = true
+            }
+        }
+    }
+    
+}
+
+
+extension CreateGroupsVC: UITextFieldDelegate {
     
 }
---
+++ Breakpoint/View/UserCell.swift
@@ -0,0 +1,47 @@
+//
+//  UserCell.swift
+//  Breakpoint
+//
+//  Created by gdm on 12/13/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    var showing = false
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLabel.text = email
+        if isSelected {
+            checkImage.isHidden = false
+        } else {
+            checkImage.isHidden = true
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            if showing == false {
+                checkImage.isHidden = false
+                showing = true
+            } else {
+                checkImage.isHidden = true
+                showing = false
+            }
+            
+        }
+    }
+}
